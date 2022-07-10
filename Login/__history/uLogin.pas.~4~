24a46970bce716ffe0ab5ec569277401d070cf1c
MiriamHeirich <franheirich5@gmail.com>
2022-07-10 17:40:49 -0300

    first commit

---
+++ Login/__history/uLogin.pas.~4~
@@ -0,0 +1,43 @@
+unit uLogin;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
+
+type
+  TfrmLogin = class(TForm)
+    pnlFundo: TPanel;
+    pnlLateral: TPanel;
+    lblBemVIndo: TLabel;
+    lblMfhLocadora: TLabel;
+    lblDesenvovedor: TLabel;
+    Panel1: TPanel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    lblVersao: TLabel;
+    Image1: TImage;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    lblDigiteUsuario: TLabel;
+    pnlNome: TPanel;
+    lblNome: TLabel;
+    edtNome: TEdit;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmLogin: TfrmLogin;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ Login/__recovery/uLogin.pas
@@ -0,0 +1,49 @@
+unit uLogin;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
+
+type
+  TfrmLogin = class(TForm)
+    pnlFundo: TPanel;
+    pnlLateral: TPanel;
+    lblBemVIndo: TLabel;
+    lblMfhLocadora: TLabel;
+    lblDesenvovedor: TLabel;
+    Panel1: TPanel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    lblVersao: TLabel;
+    Image1: TImage;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    lblDigiteUsuario: TLabel;
+    pnlNome: TPanel;
+    lblNome: TLabel;
+    edtNome: TEdit;
+    procedure Image1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmLogin: TfrmLogin;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmLogin.Image1Click(Sender: TObject);
+begin
+
+end;
+
+end.
